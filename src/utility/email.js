bb4eafa7d23e51c7513ca6a7885ed67a801bcab6
“Zahed75” <zahedhasan.cs@gmail.com>
2024-06-02 10:53:29 +0600

    smtp updated for the customer

--- src/utility/email.js
+++ src/utility/email.js
@@ -60,29 +60,29 @@ exports.otpMail = async (email, otp) => {
 
 
 
-// exports.otpMail = async(email,otp)=>{
+exports.otpMail = async(email,otp)=>{
 
-//   const transporter = nodemailer.createTransport({
-//       service: 'gmail',
-//       auth: {
-//           user: 'shahriartasin2000@gmail.com',
-//           pass: process.env.EMAIL_PASS
-//       }
-//   });
+  const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+          user: 'shahriartasin2000@gmail.com',
+          pass: process.env.EMAIL_PASS
+      }
+  });
 
-//   const mailOptions = {
-//       from: 'tech.syscomatic@gmail.com',
-//       to: email,
-//       subject: 'Otp for forget pass',
-//       html: `
-//           <p>Hello ,</p>
-//           <p>Welcome to our platform! your otp is ${otp}</p>
+  const mailOptions = {
+      from: 'tech.syscomatic@gmail.com',
+      to: email,
+      subject: 'Otp for forget pass',
+      html: `
+          <p>Hello ,</p>
+          <p>Welcome to our platform! your otp is ${otp}</p>
           
-//       `
-//   };
-//   await transporter.sendMail(mailOptions);
-//   // res.status(200).json({ message: 'Email sent successfully' });
-// }
+      `
+  };
+  await transporter.sendMail(mailOptions);
+  // res.status(200).json({ message: 'Email sent successfully' });
+}
 
 
 
