b763272e332577e69955fd4691198745591ffd75
tasincodes <md.shahrier.tasin@g.bracu.ac.bd>
2024-06-04 10:22:24 +0600

    get category by id up

--- src/modules/Category/service.js
+++ src/modules/Category/service.js
@@ -130,17 +130,6 @@ const getCategoryById = async(categoryId)=>{
 }
 
 
-const getCategoryById = async (id) => {
-    try {
-        const category = await Category.findById({_id:id});
-        if (!category) {
-            throw new Error('Category not found');
-        }
-        return category;
-    } catch (error) {
-        throw new Error(error.message);
-    }
-};
 
 
 module.exports ={
